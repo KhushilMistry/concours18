9b1cc9f03fcc4549f0b54de4a3806367289a1af7
Khushil Mistry <khushil324@gmail.com>
2018-08-20 01:16:41 +0530

    Home Page updated with sounds, icons and Menu. To do : Update Logo.

--- JS/index.js
+++ JS/index.js
@@ -175,4 +175,34 @@ window.onload = function () {
   $("#icon").fadeOut("slow");
   $("#container-div").fadeIn("slow");
   setTimeout(update, 500);
+  playAudio();
+}
+
+var musicDiv = document.getElementById("myAudio");
+
+function playAudio() {
+  document.getElementById("volume-off").style.display = "none";
+  document.getElementById("volume-on").style.display = "block";
+  musicDiv.play();
+}
+
+function pauseAudio() {
+  console.log('Here1');
+  document.getElementById("volume-off").style.display = "block";
+  document.getElementById("volume-on").style.display = "none";
+  console.log('Here2');
+  musicDiv.pause();
+}
+
+musicDiv.addEventListener('ended', function() {
+  this.currentTime = 0;
+  this.play();
+}, false);
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
