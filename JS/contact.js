8bd69ca03e933d313de528b4bcdb0857ffb03336
Khushil Mistry <khushil324@gmail.com>
2018-08-27 12:38:54 +0530

    Contact us page updated.

--- JS/contact.js
+++ JS/contact.js
@@ -10,3 +10,22 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+function myMap() {
+  var icon = {
+    url: './assets/images/logo.png',
+    scaledSize: new google.maps.Size(50, 50),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(0, 0)
+  };
+  var mapCanvas = document.getElementById("map");
+  var myCenter = new google.maps.LatLng(23.1885469, 72.6290294);
+  var mapOptions = { center: myCenter, zoom: 16 };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({
+    position: myCenter,
+    animation: google.maps.Animation.BOUNCE,
+    icon : icon
+  });
+  marker.setMap(map);
+}
