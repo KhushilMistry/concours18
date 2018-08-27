46ac940d1a8db7fd0261c8a46bbfafb2c204023e
Khushil Mistry <khushil324@gmail.com>
2018-08-28 00:26:09 +0530

    Code cleaned and events page added.

--- JS/faq.js
+++ JS/faq.js
@@ -1,16 +1,3 @@
-window.onload = function () {
-  $("#icon").fadeOut("slow");
-  $("#container-div").fadeIn("slow");
-}
-
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
-
 $(document).ready(function(){
   $(".box").click(function(){
     $(this).next().slideToggle("fast");
